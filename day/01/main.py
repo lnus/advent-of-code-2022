7673195176553ec23da0f68fbc47a419dacb0b30
Linus Wähler <linus.wahler@gmail.com>
2022-12-02 00:00:30 +0100

    updated day1 to solve both

--- day/01/main.py
+++ day/01/main.py
@@ -1,3 +1,5 @@
-topwhatever = 3
-print(sum(sorted([sum(map(int, elf)) for elf in [list(filter(None, line.split("\n")))
-      for line in open('input.txt', 'r').read().split("\n\n")]], reverse=True)[:topwhatever]))
+top1, top2, top3 = (sorted([sum(map(int, elf)) for elf in [list(filter(None, line.split("\n")))
+                                                           for line in open('input.txt', 'r').read().split("\n\n")]], reverse=True)[:3])
+
+print("Top 1:", top1)
+print("Top 3:", sum([top1, top2, top3]))
